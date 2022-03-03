d0b5c9e5e887767c65afab370efa02326cfabb36
erkn <erkn@fysik.dtu.dk>
2022-03-03 09:33:39 +0100

    skeleton for tool

---
+++ mcpl2openmc.c
@@ -0,0 +1,26 @@
+#include <math.h>
+#include <cstdio>
+#include <openmc/hdf5_interface>
+#include <mcpl.h>
+
+int main (int argc, char *argv[]){
+
+  /* Extract filename from input arguments.*/
+
+  /* open the input MCPL-file for reading*/
+
+  /* open the openmc output file */
+  
+
+  /*loop over particles, ignoring ones that are not available in openmc.*/
+
+
+
+
+
+}
+
+
+
+
+
