c28638205df08e2f0225f0602895946bd6a07358
erkn <erkn@fysik.dtu.dk>
2022-03-04 01:23:00 +0100

    WIP copied in some code from openmc for h5 file writing

--- mcpl2openmc.c
+++ mcpl2openmc.c
@@ -80,4 +80,101 @@ int main (int argc, char *argv[]){
 
 
 
+void write_source_point(const char* filename, bool surf_source_bank)
+{
+  // When using parallel HDF5, the file is written to collectively by all
+  // processes. With MPI-only, the file is opened and written by the master
+  // (note that the call to write_source_bank is by all processes since slave
+  // processes need to send source bank data to the master.
+
+  std::string filename_;
+  if (filename) {
+    filename_ = filename;
+  } else {
+    // Determine width for zero padding
+    int w = std::to_string(settings::n_max_batches).size();
+
+    filename_ = fmt::format("{0}source.{1:0{2}}.h5", settings::path_output,
+      simulation::current_batch, w);
+  }
+
+  hid_t file_id;
+  file_id = file_open(filename_, 'w', true);
+  write_attribute(file_id, "filetype", "source");
+
+  // Get pointer to source bank and write to file
+  write_source_bank(file_id, surf_source_bank);
+  
+  file_close(file_id);
+}
+
+/*This bit is cpoied over from openmc*/
+
+
+void write_source_bank(hid_t group_id, bool surf_source_bank)
+{
+  hid_t banktype = h5banktype();
+
+  // Set total and individual process dataspace sizes for source bank
+  int64_t dims_size = settings::n_particles;
+  int64_t count_size = simulation::work_per_rank;
+
+  // Set vectors for source bank and starting bank index of each process
+  vector<int64_t>* bank_index = &simulation::work_index;
+  vector<SourceSite>* source_bank = &simulation::source_bank;
+  vector<int64_t> surf_source_index_vector;
+  vector<SourceSite> surf_source_bank_vector;
+
+  // Reset dataspace sizes and vectors for surface source bank
+  if (surf_source_bank) {
+    surf_source_index_vector = calculate_surf_source_size();
+    dims_size = surf_source_index_vector[mpi::n_procs];
+    count_size = simulation::surf_source_bank.size();
+
+    bank_index = &surf_source_index_vector;
+
+    // Copy data in a SharedArray into a vector.
+    surf_source_bank_vector.resize(count_size);
+    surf_source_bank_vector.assign(simulation::surf_source_bank.data(),
+      simulation::surf_source_bank.data() + count_size);
+    source_bank = &surf_source_bank_vector;
+  }
+
+  if (mpi::master) {
+    // Create dataset big enough to hold all source sites
+    hsize_t dims[] {static_cast<hsize_t>(dims_size)};
+    hid_t dspace = H5Screate_simple(1, dims, nullptr);
+    hid_t dset = H5Dcreate(group_id, "source_bank", banktype, dspace,
+      H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+    // Save source bank sites since the array is overwritten below
+
+    for (int i = 0; i < mpi::n_procs; ++i) {
+      // Create memory space
+      hsize_t count[] {
+        static_cast<hsize_t>((*bank_index)[i + 1] - (*bank_index)[i])};
+      hid_t memspace = H5Screate_simple(1, count, nullptr);
+
+      // Select hyperslab for this dataspace
+      dspace = H5Dget_space(dset);
+      hsize_t start[] {static_cast<hsize_t>((*bank_index)[i])};
+      H5Sselect_hyperslab(
+        dspace, H5S_SELECT_SET, start, nullptr, count, nullptr);
+
+      // Write data to hyperslab
+      H5Dwrite(
+        dset, banktype, memspace, dspace, H5P_DEFAULT, (*source_bank).data());
+
+      H5Sclose(memspace);
+      H5Sclose(dspace);
+    }
+
+    // Close all ids
+    H5Dclose(dset);
+
+  } else {
+  }
+
+  H5Tclose(banktype);
+}
 
